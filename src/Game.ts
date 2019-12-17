ffbf0ce9dd5348ce45b73413da37aa6c01a4a0ad
maciar2000 <kontakt@maciejadamczyk.com.pl>
2019-12-17 10:19:03 +0100

    path color

--- src/Game.ts
+++ src/Game.ts
@@ -10,7 +10,7 @@ class Game {
     }
     create3Balls(): void {
         let ball = new Ball();
-        let x: number = ball.checkEmptyField() >= 3 ? 3 : ball.checkEmptyField();
+        let x: number = ball.checkEmptyField() >= 3 ? 21 : ball.checkEmptyField();
         for (let i = 0; i < x; i++) {
             ball = new Ball();
             ball.create();
@@ -43,10 +43,11 @@ class Game {
             item.addEventListener('mouseover', function () {
                 document.querySelector('.hoverField') ? document.querySelector('.hoverField').setAttribute('class', 'field') : null;
                 this.setAttribute('class', 'field hoverField');
+                set('fieldId', this.id);
+                //searchPath()
             });
             item.addEventListener('click', function () {
                 set('fieldId', this.id);
-                this.style.background = 'white';
                 if (searchPath()) moveBall();
                 else console.log('nothing')
             });
@@ -57,23 +58,24 @@ class Game {
         this.board = JSON.parse(JSON.stringify(settings.board));
         const [xBall, yBall] = settings.ballId.split(',');
         const [xField, yField] = settings.fieldId.split(',');
-        console.log('ball', xBall, yBall);
-        console.log('field', xField, yField);
-        console.log(settings.board);
+        // console.log('ball', xBall, yBall);
+        // console.log('field', xField, yField);
+        // console.log(settings.board);
         let neighboursBall = this.getNeighbours(eval(xBall), eval(yBall));
         let currentField = `${xBall},${yBall}`;
-        console.info(neighboursBall, this.board)
+        // console.info(neighboursBall, this.board)
         let i = 1;
         let j = 1;
         let key = 0;
         let neighboursBase = [...neighboursBall];
         for (const item of neighboursBall) {
             const [x, y] = item.split(',');
+            this.board[eval(y)][eval(x)] = j;
             currentField = `${x},${y}`;
             if (currentField == `${xField},${yField}`) break;
             const neighbours = this.getNeighbours(eval(x), eval(y));
-            this.board[eval(y)][eval(x)] = j;
             document.getElementById(`${x},${y}`).innerHTML += `${j}`;
+
             for (const item2 of neighbours) {
                 if (neighboursBall.indexOf(item2) == -1)
                     neighboursBall.push(item2);
@@ -81,15 +83,53 @@ class Game {
             }
             if (neighboursBase.length / i == 1) {
                 key += i;
-                console.log(j, i, neighboursBase, neighboursBase.slice(key));
+                // console.log(j, i, neighboursBase, neighboursBase.slice(key));
                 neighboursBase = neighboursBall.slice(key);
                 j++;
                 i = 1;
             } else i++;
         }
+        // console.log(neighboursBase, neighboursBall)
+        let path: String[][] = [[], []];
+        console.log(j)
+        console.table(this.board)
+        let x: number = xField;
+        let y: number = yField;
+        let val: number = j;
+        for (let i = 0; ; i++){
+            let neighbours = this.getNeighbours(x, y, true);
+            console.log('s',i,neighbours)
+            let index = 0;
+            for (let item of neighbours) {
+                console.log(item)
+                let x2: number = eval(item.split(',')[0]);
+                let y2: number = eval(item.split(',')[1]);
+                if (this.board[y2][x2] == val - 1) {
+                    path[index][i] = item;
+                    index++;
+                    x = x2;
+                    y = y2;
+                }
+            }
+            if (val == 0) break;
+            val -= 1;
+        }
+        if (path[1].length > 0) {
+            for (let id in path[1]) {
+                path[0][id] = path[1][id];
+            }
+        }
+        path[0].unshift(`${xField},${yField}`);
+        console.log(path)
+        for (let item of path[0]) {
+            setTimeout(() => {
+                
+                document.getElementById(<string>item).setAttribute('class', 'field path');
+            }, 100);
+        }
         return true;
     }
-    getNeighbours(x1: number, y1: number): Array<String> {
+    getNeighbours(x1: number, y1: number,filled?:boolean): Array<String> {
         let neighbours: Array<String> = [];
         for (let x: number = Math.max(0, x1 - 1); x <= Math.min(x1 + 1, settings.size - 1); x++) {
             for (let y: number = Math.max(0, y1 - 1); y <= Math.min(y1 + 1, settings.size - 1); y++) {
@@ -106,7 +146,7 @@ class Game {
                         )
                     )
                 ) {
-                    if (this.board[y][x] == 0)
+                    if ((!filled && this.board[y][x] == 0) || filled)
                         neighbours.push(`${x},${y}`)
                 }
             }
